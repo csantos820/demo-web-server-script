b72a4b1c28f54acd4c796e8e7e4d8ea274be2eac
root <root@ip-172-31-37-0.ec2.internal>
2019-05-12 17:46:01 +0000

    page update

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install nginx git nodejs npm -fy
 
-systemctl status nginx.service
+#systemctl status nginx.service
 
 cd /var/www/
 
@@ -15,4 +15,5 @@ npm i forever -g
 forever start app.js
 
 cd /var/www/html/
-wget  https://github.com/csantos820/demosite/plain-html/index.html
+wget https://raw.githubusercontent.com/csantos820/demosite/master/plain-html/index.html
+
